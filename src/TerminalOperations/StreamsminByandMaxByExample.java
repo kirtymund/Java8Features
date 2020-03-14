f4ff25d9cceb280919b2b2b7c3803e455d8650ff
Purohit <Purohit@192.168.0.20>
2020-03-14 08:51:25 +0530

    MinBy() and MaxBy() methods as parameter to collect(). Returns Optional<T> and input as Comparator.(Comparator.comparing(Function<T,R>))

---
+++ src/TerminalOperations/StreamsminByandMaxByExample.java
@@ -0,0 +1,29 @@
+package TerminalOperations;
+
+import StudentData.Student;
+import StudentData.StudentDataBase;
+import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.minBy;
+
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collector;
+
+
+public class StreamsminByandMaxByExample {
+
+	public static Optional<Student> minByExample() {
+		return StudentDataBase.getAllStudents().stream().collect(minBy(Comparator.comparing(Student::getGpa)));
+	}
+
+	
+	public static Optional<Student> maxByExample() {
+		return StudentDataBase.getAllStudents().stream().collect(maxBy(Comparator.comparing(Student::getGpa)));
+	}
+
+	public static void main(String[] args) {
+		System.out.println(minByExample());
+		System.out.println(maxByExample());
+	}
+
+}
